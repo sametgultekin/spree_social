40a35ced9f915403435d7b3e0bbbe04cb7b415cd
Samet Gultekin <samet.gltkn@gmail.com>
2013-11-26 22:16:16 +0200

    fixed translation namespace issue

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         ssl_allowed :#{provider}
         def #{provider}
           if request.env["omniauth.error"].present?
-            flash[:error] = t("devise.omniauth_callbacks.failure", :kind => auth_hash['provider'], :reason => t(:user_was_not_valid))
+            flash[:error] = Spree.t("devise.omniauth_callbacks.failure", :kind => auth_hash['provider'], :reason => Spree.t(:user_was_not_valid))
             redirect_back_or_default(root_url)
             return
           end
@@ -18,18 +18,18 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
 
           if authentication.present?
-            flash[:notice] = t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
+            flash[:notice] = Spree.t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
             sign_in_and_redirect :spree_user, authentication.user
           elsif spree_current_user
             spree_current_user.apply_omniauth(auth_hash)
             spree_current_user.save!
-            flash[:notice] = t("devise.sessions.signed_in")
+            flash[:notice] = Spree.t("devise.sessions.signed_in")
             redirect_back_or_default(account_url)
           else
             user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
             user.apply_omniauth(auth_hash)
             if user.save
-              flash[:notice] = t("devise.registrations.signed_up")
+              flash[:notice] = Spree.t("devise.registrations.signed_up")
               sign_in_and_redirect :spree_user, user
             else
               session[:omniauth] = auth_hash.except('extra')
